1c3b8fc20a7fdb7c252f9361405dbb001fb8355a
Aaron Belmore <belmoreaaron9@gmail.com>
2023-08-18 13:23:28 -0400

    created model

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -14,4 +14,22 @@ router.get("/", (req, res) => {
       res.json(err);
     });
 });
+
+router.get("/:id", (req, res, next) => {
+  Cars.getById(req.params.id)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/", (req, res) => {
+  Cars.create(req.body)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 module.exports = router;
